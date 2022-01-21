77e18df5f2dc0b47081a35cadd6e6ebe1ec2f342
Ria Sehgal <riasehgal48@gmail.com>
2022-01-21 10:06:20 +0530

    Update Max_element_of_array.cpp

--- Max_element_of_array.cpp
+++ Max_element_of_array.cpp
@@ -21,3 +21,23 @@ int main()
 
     return 0;
 }
+
+//Direct Way
+
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{   
+    int arr[] = {10, 20, 100, 40, 50, 500, 8};
+    
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    int res = *max_element(arr, arr+n);
+    
+    cout<<res;
+    
+    return 0;
+}
