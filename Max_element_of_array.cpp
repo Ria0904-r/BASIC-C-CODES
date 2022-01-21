594fe014c5e70d386e2a00c1d8789b6db2b73462
Ria Sehgal <riasehgal48@gmail.com>
2022-01-21 18:58:47 +0530

    Update Max_element_of_array.cpp

--- Max_element_of_array.cpp
+++ Max_element_of_array.cpp
@@ -41,3 +41,19 @@ int main()
     
     return 0;
 }
+
+//Vector Method
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    vector<int> v {10,30,23,45,67,23,678,345,347,567};
+    int res= *max_element(v.begin(),v.end());
+    cout<<res<<endl;
+
+    return 0;
+}
