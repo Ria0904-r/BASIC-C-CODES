f04cc2a387b0ceeed5d1ef6ecf21051148ec9390
Ria Sehgal <riasehgal48@gmail.com>
2022-04-10 19:19:36 +0530

    Update functiontemplate.cpp

--- functiontemplate.cpp
+++ functiontemplate.cpp
@@ -1,9 +1,11 @@
+//Changes which have to do while setting a limit are written in comments//
+
 #include <iostream>
 #include <vector>
 
 using namespace std;
-template<typename T>
-T arrMax(T arr[],int n){
+template<typename T>     //template<typenameT,int limit>
+T arrMax(T arr[],int n){   //if(n>limit){ } //         
     T res=arr[0];
     for(int i=1;i<n;i++){
         if(arr[i]>res)
@@ -18,7 +20,7 @@ T arrMax(T arr[],int n){
   int main(){
       int arr1[]={10,34,67};
       
-   cout<<arrMax<int>(arr1,3)<<endl;
+   cout<<arrMax<int>(arr1,3)<<endl;       //<int,100>(arr1,3)//
    float arr2[]={56,78,21,76,34,230};
    cout<<arrMax<float>(arr2,6)<<endl;
    
