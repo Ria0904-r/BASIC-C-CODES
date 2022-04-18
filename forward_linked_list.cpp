7234a74631d987c5e3cef8885eae07205a45737c
Ria Sehgal <riasehgal48@gmail.com>
2022-04-18 19:16:41 +0530

    Create forward_linked_list.cpp

---
+++ forward_linked_list.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <forward_list>
+
+using namespace std;
+
+int main()
+{
+    forward_list<int> l={10,15,20};
+    l.push_front(5);
+    l.push_front(3);
+    l.pop_front();
+    
+    for(int x:l){
+        cout<<x<<" ";
+    }
+
+    return 0;
+}
